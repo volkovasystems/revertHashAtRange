5c8455b514b99e927494d71eac0e09f516adf42f
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-08-04 05:50:01 +0800

    Initial commit with reverHashAtRange.java

---
+++ revertHashAtRange.java
@@ -0,0 +1,13 @@
+package revertHashAtRange;
+
+
+
+public class revertHashAtRange{
+	public static void main( String... parameterList ){
+
+	}
+
+	public static final String revertHashAtRange( ){
+
+	}
+}
